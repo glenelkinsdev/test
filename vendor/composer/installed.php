1eb8404d9b874e5586fd8cde50fc0ef7269a43c8
Glen Elkins <glenelkinsdev@gmail.com>
2022-02-13 09:49:22 +0000

    Cleanup

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '47b0c5c0286aea35a0b88b832d63e6c3f675015c',
+        'name' => 'totallywicked/test',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'totallywicked/test' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '47b0c5c0286aea35a0b88b832d63e6c3f675015c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
